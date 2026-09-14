b2a64dffaab74bb7c7b91f2bb383750a81ce6ad7
mppomere68 <owczarek.antek@gmail.com>
2026-09-14 19:16:39 +0200

    czy trójkąt/ jaki rodzaj trójkąta

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-# a = float(input("a = "))
-# b = float(input("b = "))
-# c = float(input("c = "))
-# if a + b > c:
-#     if a + c > b:
-#         if b + c > a:
-#             print('tak')
-#         else:
-#             print("nie")
-#     else:
-#         print('nie')    
-# else:
-#     print('nie')        
+#czy to trojkat
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+if a + b > c:
+    if a + c > b:
+        if b + c > a:
+            print('tak')
+        else:
+            print("nie")
+    else:
+        print('nie')    
+else:
+    print('nie')        
 
+
+#jaki rodzaj trojkata
 a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
 if a + b > c and a + c > b and b + c > a:
-    print("tak")
+    if a == b and a == b and b == c:
+        print('to jest troj rownoramieny')
+    elif a == b or b == c or a == c:
+        if a**2 + b**2 == c**2 or b**2 + c**2 == a**2 or a**2 + c**2 == b**2:
+            print("to jest troj rownoramieny prostokatny")
+        else:
+            print("to jest troj rownoramieny")
+    elif a**2 + b**2 == c**2 or b**2 + c**2 == a**2 or a**2 + c**2 == b**2:
+        print("to jest troj prostokatny roznoboczny")
+    else:
+        print("to jest troj ruznoboczny")
+        
 else:
     print("nie")
